c43d5888b76101fc2a66769159ba9f8d613740c7
Marine <marine.magnin73@gmail.com>
2024-02-27 13:06:25 +0100

    Build : ajout styles dans index.js

--- src/index.js
+++ src/index.js
@@ -5,14 +5,31 @@ import { store, persistor } from './store/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 
+const styles = {
+  '*': {
+    boxSizing: 'border-box',
+  },
+  body: {
+    margin: 0,
+    fontFamily: `-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu',
+        'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif`,
+    WebkitFontSmoothing: 'antialiased',
+  },
+  code: {
+    fontFamily: `source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace`,
+  },
+};
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Suspense fallback={<div>Loading</div>}>
-          <App />
-        </Suspense>
-      </PersistGate>
-    </Provider>
+    <div style={styles.body}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Suspense fallback={<div>Loading</div>}>
+            <App />
+          </Suspense>
+        </PersistGate>
+      </Provider>
+    </div>
   </React.StrictMode>
 );
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,24 +1,9 @@
 import { Outlet } from "react-router-dom";
 
-const styles = {
-    '*': {
-      boxSizing: 'border-box',
-    },
-    body: {
-      margin: 0,
-      fontFamily: `-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu',
-          'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif`,
-      WebkitFontSmoothing: 'antialiased',
-    },
-    code: {
-      fontFamily: `source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace`,
-    },
-  };
-
 export default function Root() {
     return (
         <>
-            <div style={styles.body}>
+            <div >
                 <Outlet />
             </div>
         </>
