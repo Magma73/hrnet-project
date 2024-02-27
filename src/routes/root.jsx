6d4ac7eaeb4e58af5d9563eeedf59b7e70af2805
Marine <marine.magnin73@gmail.com>
2024-02-27 11:19:56 +0100

    Build : test styles sur root

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,9 +1,24 @@
 import { Outlet } from "react-router-dom";
 
+const styles = {
+    '*': {
+      boxSizing: 'border-box',
+    },
+    body: {
+      margin: 0,
+      fontFamily: `-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu',
+          'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif`,
+      WebkitFontSmoothing: 'antialiased',
+    },
+    code: {
+      fontFamily: `source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace`,
+    },
+  };
+
 export default function Root() {
     return (
         <>
-            <div>
+            <div style={styles.body}>
                 <Outlet />
             </div>
         </>
