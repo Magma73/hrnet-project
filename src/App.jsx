11bbf235377734d339c0643ad9b843943c5063a3
Marine <marine.magnin73@gmail.com>
2024-03-01 18:50:58 +0100

    Build : suppression lazy loading sur page Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import * as React from "react";
 import { lazy, Suspense } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-const Home = lazy(() => import('./pages/Home'));
+import Home from "./pages/Home";
 const EmployeeList = lazy(() => import('./pages/EmployeeList'));
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: (
-      <Suspense fallback={<div>Loading</div>}>
-        <Home />
-      </Suspense>
-    ),
+    element: <Home />,
   },
   {
     path: '/employeelist',
