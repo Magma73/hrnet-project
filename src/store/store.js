a89c7d6cd2027b45a50a699ace1be6d6a1216934
Marine <marine.magnin73@gmail.com>
2024-03-01 08:41:07 +0100

    Build : suppression plugin redux-persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,23 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-
 import employeeInfosReducer from '../slices/employeeInfos';
 
-const persistConfig = {
-  key: 'root',
-  storage,
-};
-
-const persistedReducer = persistReducer(persistConfig, employeeInfosReducer);
-
 export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-    }),
+  reducer: {
+    employeeInfos: employeeInfosReducer,
+  },
   devTools: true,
 });
-
-export const persistor = persistStore(store);
