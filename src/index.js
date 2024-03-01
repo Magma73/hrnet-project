6b711e204bb506aba59dfb7deae3870fe8c1f15d
Marine <marine.magnin73@gmail.com>
2024-03-01 18:46:07 +0100

    Build : test index.js et App.jsx

--- src/index.js
+++ src/index.js
@@ -1,35 +1,10 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 import App from './App';
 
-const styles = {
-  '*': {
-    boxSizing: 'border-box',
-  },
-  body: {
-    margin: 0,
-    fontFamily: `-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu',
-        'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif`,
-    WebkitFontSmoothing: 'antialiased',
-  },
-  code: {
-    fontFamily: `source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace`,
-  },
-};
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <div style={styles.body}>
-//       <Provider store={store}>
-//         {/* <Suspense fallback={<div>Loading</div>}> */}
-//           <App />
-//         {/* </Suspense> */}
-//       </Provider>
-//     </div>
-//   </React.StrictMode>
-// );
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
