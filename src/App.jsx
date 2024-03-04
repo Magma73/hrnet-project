30ead6f78ca2cfcb0b4d9a67e26e038b2b8ad229
Marine <marine.magnin73@gmail.com>
2024-03-04 09:42:24 +0100

    Build : ajout css à index.js

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { lazy, Suspense } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 const EmployeeList = lazy(() => import('./pages/EmployeeList'));
+const Error = lazy(() => import('./pages/Error'));
 
 const router = createBrowserRouter([
   {
@@ -14,6 +15,14 @@ const router = createBrowserRouter([
     element: (
       <Suspense fallback={<div>Loading</div>}>
         <EmployeeList />
+      </Suspense>
+    ),
+  },
+  {
+    path: '*',
+    element: (
+      <Suspense fallback={<div>Loading</div>}>
+        <Error />
       </Suspense>
     ),
   },
