522b2cb1d76381657c9eb44652ef3144facae64d
Marine <marine.magnin73@gmail.com>
2024-05-01 10:23:49 +0200

    Build : modif configuration craco

--- craco.config.js
+++ craco.config.js
@@ -1,17 +1,9 @@
 const path = require('path');
 const { whenProd } = require('@craco/craco');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlCriticalWebpackPlugin = require('html-critical-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackInjectPreload = require('@principalstudio/html-webpack-inject-preload');
 
 module.exports = {
   webpack: {
-    entry: 'index.js',
-    output: {
-      path: __dirname + '/build',
-      filename: 'index_bundle.js',
-    },
     configure: (webpackConfig) => {
       return {
         ...webpackConfig,
@@ -19,7 +11,6 @@ module.exports = {
           ...webpackConfig.plugins,
           ...whenProd(
             () => [
-              new MiniCssExtractPlugin(),
               new HtmlCriticalWebpackPlugin({
                 base: path.resolve(__dirname, 'build'),
                 src: 'index.html',
@@ -33,23 +24,10 @@ module.exports = {
                   blockJSRequests: false,
                 },
               }),
-              new HtmlWebpackInjectPreload({
-                files: [
-                  {
-                    match: /chunk\.[a-z-0-9]*.css$/,
-                    attributes: { as: 'style' },
-                  },
-                  {
-                    match: /chunk\.[a-z-0-9]*.js$/,
-                    attributes: { as: 'script' },
-                  },
-                ],
-              }),
             ],
             []
           ),
         ],
-        
       };
     },
   },
